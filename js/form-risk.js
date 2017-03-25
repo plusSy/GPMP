d380f893147efa44135b10819999a330d5b475ec
石一龙 <yilong.shi@hand-china.com>
2017-03-25 12:19:14 +0800

    修改form-measure

--- js/form-risk.js
+++ js/form-risk.js
@@ -4,19 +4,22 @@
  */
 $(function(){
     //添加模版
-    var newTr = "<tr><td><input type='text'></td><td><select  style='width: 80px;'><option value='0'>请选择</option><option value='1'>临时</option> <option value='2'>长久</option></select></td>"+
-            "<td><input type='text'></td><td><select name='' style='width: 120px;'>"+ "<option value='0'>请选择</option></select></td><td style='color: #0560b4;'>张三</td><td><input type='text'></td>"+
+    var newTr = "<tr><td><input type='text'></td>"+
+            "<td><input type='text'></td><td><select name='' style='width: 120px;'>"+ "<option value='0'>请选择</option></select></td><td style='color: #0560b4;'><select  style='width: 80px;'><option value='0'>请选择</option><option value='1'>张三</option> <option value='2'>李四</option></select></td><td><input type='text'></td>"+
             "<td><input type='text'></td><td><div class='statues-choice'></div></td><td><input type='checkbox'></td>"+
             "<td><button>╳</button></td></tr>";
-    $(".addRiskWay").on("click",function () {
-        //获取tbody img->td->tr->tbody
-        var a = $(this).parent().parent().parent();
-        //父结点删除
-        a.append(newTr);
-    });
+    //$(".addRiskWay").on("click",function () {
+    //    //获取tbody img->td->tr->tbody
+    //    var a = $(this).parent().parent().parent();
+    //    //父结点删除
+    //    a.append(newTr);
+    //});
     $(".riskWay tbody tbody").delegate('button', 'click', function() {
         //获取当前要删除的行 tr
         var delTr = $(this).parent().parent();
         delTr.remove();
     });
 });
+function viewmeasure(url){
+    window.open(url,'newwindow', "width=900, top=0, left=0, toolbar=no, menubar=no, scrollbars=no, resizable=no, status=no")
+}
